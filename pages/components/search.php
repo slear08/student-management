65d27f083689fb3bc3ffce5c48a6e1c3e084fb71
slear08 <88441444+slear08@users.noreply.github.com>
2022-10-25 19:32:04 +0800

    added search function  and display searched data

--- pages/components/search.php
+++ pages/components/search.php
@@ -1,13 +1,46 @@
+<?php
+    include_once("../connection/connection.php");
+
+    $conn = connection();
+
+    if(!isset($_SESSION)) {
+        session_start();
+    }
+    $errorMessage = "";
+    if(isset($_POST['search'])){
+
+        $searchID = $_POST['search-student'];
+
+        $sql = "SELECT * FROM students WHERE student_id = '$searchID' ";
+
+        $student = $conn->query($sql) or die($conn->error);
+
+        $row = $student->fetch_assoc();
+        $result = $student->num_rows;
+       
+        if($result > 0){  
+            $_SESSION['StudentID'] = $row["student_id"];
+            include_once("./components/update.php");
+            echo '<script>
+                    document.getElementById("update-form").showModal();
+                  </script>';
+        }else{
+            $errorMessage = "No result found";
+        }
+    } 
+?>
 <dialog class="modal search-form" id="search-form">
-    <div class="add-title">
+    <div class="modal-title">
         <h1>SEARCH</h1>
     </div>
     <div class="form-container">
-        <form action="">
-            <input type="text" placeholder="SEARCH STUDENT ID">
+        <form method="POST" action="<?php echo $_SERVER["PHP_SELF"];?>">
+            <input type="text" name="search-student" placeholder="SEARCH STUDENT ID" required>
+            <div><p style="color:red;"><?php echo $errorMessage?></p><div>
         </form>
+        
         <div class="submit-btn">
-            <button type="submit" id="search-btn">SEARCH</button>
+            <button type="submit" name="search" id="search-btn">SEARCH</button>
         </div>
     </div>
     <div class="close-btn">
--- pages/components/update.php
+++ pages/components/update.php
@@ -1,5 +1,33 @@
+<?php
+
+    include_once("../connection/connection.php");
+
+    $conn = connection();
+
+    if(!isset($_SESSION)) {
+        session_start();
+    }
+
+    $studentID = $_SESSION['StudentID'];
+
+    
+    $studentSql = "SELECT * FROM students where student_id ='$studentID'";
+    $student = $conn->query($studentSql) or die($conn->error());
+    $studentRow = $student->fetch_assoc();
+
+    $parentSql = "SELECT * FROM guardians where guardian_id ='$studentID'";
+    $parent = $conn->query($parentSql) or die($conn->error());
+    $parentRow = $parent->fetch_assoc();
+
+    $emailSql = "SELECT * FROM accounts where id ='$studentID'";
+    $email = $conn->query($emailSql) or die($conn->error());
+    $emailRow = $email->fetch_assoc();
+
+?>
+
+
 <dialog class="modal" id="update-form">
-    <div class="add-title">
+    <div class="modal-title">
         <h1>UPDATE DATA</h1>
     </div>
     <form action="">
@@ -9,14 +37,15 @@
             </div>
             <div class="student-form">
                 <div class="col-1">
-                    <input type="text" name="student-first-name" placeholder="FIRST NAME" required>
-                    <input type="text" name="student-id" placeholder="STUDENT ID" required>
-                    <input type="text" name="year" placeholder="YEAR LEVEL" required>
+                    <input type="text" name="student-first-name" placeholder="FIRST NAME" value="<?php echo $studentRow['firstname'];?>" required>
+                    <input type="text" name="student-id" placeholder="STUDENT ID" value="<?php echo $studentRow['student_id'];?>" required>
+                    <input type="text" name="year" placeholder="YEAR LEVEL" value="<?php echo $studentRow['year'];?>" required>
+                    <input type="text" name="email" placeholder="EMAIL@example.com" value="<?php echo $emailRow['email'];?>" required>
                 </div>
                 <div class="col-2">
-                    <input type="text" name="student-last-name" placeholder="LAST NAME" required>
-                    <input type="text" name="section" placeholder="SECTION" required>
-                    <input type="text" name="course" placeholder="COURSE" required>
+                    <input type="text" name="student-last-name" placeholder="LAST NAME" value="<?php echo $studentRow['lastname'];?>" required>
+                    <input type="text" name="section" placeholder="SECTION" value="<?php echo $studentRow['section'];?>" required>
+                    <input type="text" name="course" placeholder="COURSE" value="<?php echo $studentRow['course'];?>" required>
                 </div>
             </div>
         </div>
@@ -30,12 +59,12 @@
             </div>
             <div class="guardian-form">
                 <div>
-                    <input type="text" name="guardian-first-name" placeholder="FIRST NAME" required>
-                    <input type="text" name="guardian-last-name" placeholder="LAST NAME" required>
+                    <input type="text" name="guardian-first-name" placeholder="FIRST NAME" value="<?php echo $parentRow['firstname'];?>" required>
+                    <input type="text" name="guardian-last-name" placeholder="LAST NAME" value="<?php echo $studentRow['lastname'];?>" required>
                 </div>
                 <div>
-                    <input type="text" name="relation" placeholder="RELATION" required>
-                    <input type="text" name="contact" placeholder="CONTACT NUMBER" required>
+                    <input type="text" name="relation" placeholder="RELATION" value="<?php echo $parentRow['relation'];?>" required>
+                    <input type="text" name="contact" placeholder="CONTACT NUMBER" value="<?php echo $parentRow['contact'];?>" required>
                 </div>
             </div>
         </div>
